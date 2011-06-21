72b2eca4ceb9169844e2b1276fe4ee5fae980abe
Madhur <ahuja.madhur@gmail.com>
2011-06-20 22:47:31 -0700

    integrated linkedin

--- src/popup.js
+++ src/popup.js
@@ -1,19 +1,15 @@
-/*var backgroundPage = chrome.extension.getBackgroundPage();
-var mailAccounts = backgroundPage.accounts;
-var mailCount = 0;
-var mailCache = new Array();
-var allMail;
-var scrollbar;
-
-var unreadCount = 0;
-allMail = new Array();
-
-$.each(mailAccounts, function (i, account) {
-   unreadCount += account.getUnreadCount();
-});
-*/
-
 var miner=new SocialMiner();
+
+function EnsureFBToken()
+{
+
+		var accessToken = chrome.extension.getBackgroundPage().getItem("accesstoken");
+		console.log(JSON.parse(accessToken));
+		miner.FBAccessToken=JSON.parse(accessToken);
+		
+			    
+
+}
 
 function renderContents()
 {
@@ -25,25 +21,9 @@ function renderContents()
 $(document).ready(function () {
    
    var urlType;
+   var profileName;
    
    $("#tabs" ).tabs();
-
-  
-
-    /*window.fbAsyncInit = function() {
-        FB.init({appId: '209204245784872', status: true, cookie: true, xfbml: true});
-    };
-    (function() {
-        var e = document.createElement('script');
-        e.type = 'text/javascript';
-        e.src = "http://connect.facebook.net/en_US/all.js";
-        e.async = true;
-        document.getElementById('fb-root').appendChild(e);
-	
-
-    }());*/
-   
-  
 
    miner.getTabUrl(function(pageUrl) 
    {
@@ -58,12 +38,72 @@ $(document).ready(function () {
 		   if(urlType=="fb")
 		   {
 			console.log(urlType);
-			$( "#tabs" ).tabs("select", 2);
 			
 			
 			// Get the profile name
-			var profileName=miner.getFBProfileName(miner.tabUrl);
+			profileName=miner.getFBProfileName(miner.tabUrl);
 			console.log("Profile:"+profileName);
+			
+			// Get the access token
+			EnsureFBToken();
+			
+			if (isNaN (profileName)) 
+			{
+  
+			    miner.GetFBProfiles(function(profileArray)
+					    {
+					    // error condition, fb not logged in
+						if(profileArray==null)
+						{
+							$("#fblogin").show();
+							return;
+						
+						}
+						$("#fblogin").hide();
+						
+						    console.log(profileArray);
+						    $.each(profileArray, function(index, profileObj)
+						    {
+							    console.log(profileObj);
+							    var fbHtml=parseTemplate($("#FBTemplate").html(), { profile: profileObj});
+							    console.log(fbHtml);
+							    $(fbHtml).appendTo("#fbcontent");
+						    });
+						    
+					    }, profileName,"user");
+			    
+			    
+			} 
+			else 
+			{
+				miner.GetFBNameFromID(function(name)
+				{
+					console.log(name);
+					profileName=name;
+					
+					miner.GetFBProfiles(function(profileArray)
+					    {
+						    console.log(profileArray);
+						    $.each(profileArray, function(index, profileObj)
+						    {
+							    console.log(profileObj);
+							    var fbHtml=parseTemplate($("#FBTemplate").html(), { profile: profileObj});
+							    console.log(fbHtml);
+							    $(fbHtml).appendTo("#fbcontent");
+						    });
+						    
+					    }, profileName,"user");
+				
+				},profileName); 
+				  
+			}
+			
+			
+			
+			 
+	    
+			
+			
 			
 		   }
 		   else if(urlType=="lnkd")
@@ -76,15 +116,38 @@ $(document).ready(function () {
 			console.log(urlType);
 			$( "#tabs" ).tabs("select", 0);
 		   }
+		   else if(urlType=="goog")
+		   {
+			searchQry=miner.getSearchQueryTerm(miner.tabUrl);
+			console.log(searchQry);
+			
+			EnsureFBToken();
+			
+			 miner.GetFBProfiles(function(profileArray)
+					    {
+						// error condition, fb not logged in
+						if(profileArray==null)
+						{
+							$("#fblogin").show();
+							return;
+						
+						}
+						$("#fblogin").hide();
+						    console.log(profileArray);
+						    $.each(profileArray, function(index, profileObj)
+						    {
+							    console.log(profileObj);
+							    var fbHtml=parseTemplate($("#FBTemplate").html(), { profile: profileObj});
+							    console.log(fbHtml);
+							    $(fbHtml).appendTo("#fbcontent");
+						    });
+						    
+					    }, searchQry,"user");
+			    
 		   
-	    var accessToken = chrome.extension.getBackgroundPage().getItem("accesstoken");
-	    console.log(JSON.parse(accessToken));
-	    miner.FBAccessToken=JSON.parse(accessToken);
-	  //  console.log(accessToken);
-	    miner.GetFBProfiles("madhur","user");
-	    
-	    
-	    
+		   }
+		   
+	   
    });
 
  
--- src/socialminer.js
+++ src/socialminer.js
@@ -11,6 +11,8 @@ function SocialMiner()
 	
 	this.FBAccessToken;
 	
+	var profileArray=new Array();
+	
 	this.getTabUrl=function(callback)
 	{
 		
@@ -42,10 +44,35 @@ function SocialMiner()
 	
 	this.getFBProfileName=function(pageUrl)
 	{
-		var index=pageUrl.lastIndexOf("/");
-		var profileName=pageUrl.substring(index+1);
-		console.log("FB Profile is" + profileName);
-		return profileName;
+			
+		var re = /(?:http:\/\/)?(?:www.)?facebook\.com\/(?:profile\.php\?id=(?=\d.*))?([\w\-\.]*)?/;
+		var m = re.exec(pageUrl);
+		if (m == null) 
+		{
+			alert("No match");
+		} 
+		else 
+		{
+			var s = "Match at position " + m.index + ":\n";
+			for (i = 0; i < m.length; i++) 
+			{
+				s = s + m[i] + "\n";
+			}
+			
+		}
+		return m[1];
+	
+	
+	}
+	
+	this.getSearchQueryTerm=function(pageUrl)
+	{
+		var googRegex1=new RegExp("(?:http://www\\.google\\.(?:com?\\.)?[a-z]{2,3}/(?:search)?.*?&q=\\+*([^&\"\\r\\n]+)[^\"\\r\\n]*)");
+		var m=googRegex1.exec(pageUrl);
+		return m[1];
+	
+	
+	
 	
 	
 	}
@@ -55,11 +82,11 @@ function SocialMiner()
 	{
 		this.logToConsole(pageUrl);
 		
-		//console.log(pageUrl.prototype.toString());
+		
 		var fbRegex="(?:http:\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?";
 		var soRegex="(?:http:\/\/)?(?:www.)?stackoverflow.com\/(?:(?:\w)*#!\/)?(?:users\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?";
 		var lnkdRegex="(?:http:\/\/)?(?:www.)?linkedin.com\/(?:(?:\w)*#!\/)?(?:in\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?";
-		
+		var googRegex="(?:http://www\\.google\\.(?:com?\\.)?[a-z]{2,3}/(?:search)?.*?&q=\\+*([^&\"\\r\\n]+)[^\"\\r\\n]*)";
 		if(pageUrl.toString().match(fbRegex))
 		{
 			this.logToConsole("Its FB Page");
@@ -80,6 +107,12 @@ function SocialMiner()
 			return "lnkd";
 		
 		}
+		else if(pageUrl.toString().match(googRegex))
+		{
+			this.logToConsole("Its Goog page");
+			return "goog";
+		
+		}
 	
 	
 		
@@ -93,27 +126,81 @@ function SocialMiner()
 	}
 	
 	
-	this.GetFBProfiles=function(email,type)
+	this.GetFBProfiles=function(callback, email,type)
 	{
-		var searchQry="https://graph.facebook.com/search?q=madhur&type="+type+"&access_token=" + this.FBAccessToken;
+		 
+		var searchQry="https://graph.facebook.com/search?q="+email+"&type="+type+"&access_token=" + this.FBAccessToken;
 		
 		console.log(this.FBAccessToken);
 		
 		$.getJSON(searchQry, function(json)
 		{
+			console.log("json:"+json);
+			
+			$.each(json.data,function(index,val)
+			{
+				
+				console.log(val.id);
+				console.log(val.name);
+				var profileId=val.id;
+				var profileName=val.name;
+				
+				var profileObject = 
+				{
+				    "id": profileId,
+				    "name": profileName
+				  
+				 };
+				 
+				 
+				 
+				profileArray.push(profileObject);
+				
+				
+				console.log(profileArray.length);
+				
+				
+				
+			});
+			
+			console.log(profileArray);
+			
+			callback(profileArray);
+			
+		})
+		.success(function()
+		{
+		
+			//alert('success');
+		})
+		.error(function()
+		{
+			alert('failed');
+			callback(null);
+		
+		});
+		
+		
+		
+		
+	
+	
+	}
+	
+	
+	this.GetFBNameFromID=function(callback, id)
+	{
+	
+		var searchQry="https://graph.facebook.com/"+id;
+		
+		$.getJSON(searchQry, function(json)
+		{
 			console.log(json);
 			
-			$.each(json,function(index,val)
-			{
-				console.log(key);
-				console.log(val);
-				var profileId=val[0].id;
-				var imgQuery="https://graph.facebook.com/"+profileId+"/picture/";
-				
-				$("#fb-content").append('<p><img src="https://graph.facebook.com/'+profileId+'/picture"/>');
-			});
+			callback(json.name);
+			
 		});
-	
+		
 	
 	}
 	
